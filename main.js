fe7d40b8c29d65c3f730d43ff0758dd669a336f3
Shahriar Rhythm <rhythmshahriar0@gmail.com>
2022-04-22 19:18:41 +0600

    commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     autoplay: true,
     speed: 400,
+    mousewheel: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true
